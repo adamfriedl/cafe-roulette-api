f5e9a2c2a60fc60eb8cfbea4cb018b336c6f8527
Adam Friedl <ajfriedl@gmail.com>
2017-07-10 15:35:43 -0400

    please!

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://caferoulette.herokuapp.com/'
+    origins 'https://caferoulette.herokuapp.com'
 
     resource '*',
       headers: :any,
