a52e46f04ece3939d26cbbde41598384c5febb44
Adam Friedl <ajfriedl@gmail.com>
2017-07-11 10:08:43 -0400

    adjusted cors for dev and production

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'caferoulette.herokuapp.com'
+    origins 'caferoulette.herokuapp.com', 'localhost:3000'
 
     resource '*',
       headers: :any,
